204dbcee887ad89e9ef9406e2c56a4f0a4fee5ed
atishshingote <atishshingote@gmail.com>
2023-08-24 12:52:36 +0530

    commite

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const express = require("express")
 const path = require("path")
 const app = express()
-// const hbs = require("hbs")
+const hbs = require("hbs")
 const LogInCollection = require("./mongo")
-const port = process.env.PORT || 3000
+const port = 4000 ;
 app.use(express.json())
 
 app.use(express.urlencoded({ extended: false }))
@@ -16,84 +16,56 @@ app.set('view engine', 'hbs')
 app.set('views', tempelatePath)
 app.use(express.static(publicPath))
 
-
-// hbs.registerPartials(partialPath)
-
-
-app.get('/signup', (req, res) => {
-    res.render('signup')
-})
 app.get('/', (req, res) => {
     res.render('login')
 })
 
-
-
-// app.get('/home', (req, res) => {
-//     res.render('home')
-// })
+app.get('/signup', (req, res) => {
+    res.render('signup')
+})
 
 app.post('/signup', async (req, res) => {
-    
-    // const data = new LogInCollection({
-    //     name: req.body.name,
-    //     password: req.body.password
-    // })
-    // await data.save()
 
-    const data = {
+    const data = new LogInCollection({
         name: req.body.name,
         password: req.body.password
-    }
-
-    const checking = await LogInCollection.findOne({ name: req.body.name })
-
-   try{
-    if (checking.name === req.body.name && checking.password===req.body.password) {
-        res.send("user details already exists")
-    }
-    else{
-        await LogInCollection.insertMany([data])
-    }
-   }
-   catch{
-    res.send("wrong inputs")
-   }
-
-    res.status(201).render("home", {
-        naming: req.body.name
     })
+    await data.save()
+
+    res.send('User created successfully');
+
+
+
+
+    app.post('/login', async (req, res) => {
+
+        try {
+            const check = await LogInCollection.findOne({ name: req.body.name })
+
+            if (check.password === req.body.password) {
+                res.status(201).render("home", { naming: `${req.body.name}` })
+            }
+
+            else {
+                res.send("incorrect password")
+            }
+
+
+        }
+
+        catch (e) {
+
+            res.send("wrong details")
+
+
+        }
+
+
+    })
+
 })
 
-
-app.post('/login', async (req, res) => {
-
-    try {
-        const check = await LogInCollection.findOne({ name: req.body.name })
-
-        if (check.password === req.body.password) {
-            res.status(201).render("home", { naming: `${req.body.password}+${req.body.name}` })
-        }
-
-        else {
-            res.send("incorrect password")
-        }
-
-
-    } 
+    app.listen(port, () => {
+        console.log('port connected');
+    })
     
-    catch (e) {
-
-        res.send("wrong details")
-        
-
-    }
-
-
-})
-
-
-
-app.listen(port, () => {
-    console.log('port connected');
-})
--- src/mongo.js
+++ src/mongo.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose")
-
+mongoose.set('strictQuery', true);
 mongoose.connect("mongodb://localhost:27017/LoginFormPractice")
 .then(()=>{
     console.log('mongoose connected');
